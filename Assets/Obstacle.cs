57bf9da0af65317a760443591f8c164e80a8d09b
Areeb Mohammed <areebmohammed11@gmail.com>
2021-04-26 02:14:51 -0500

    Simple level system implemented

---
+++ Assets/Obstacle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public int PointPenalty;
+    public bool KillPlayer;
+    public bool RespawnObstacle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameManager.AddPoints(-PointPenalty);
+        if (KillPlayer)
+            GameManager.KillPlayer();
+
+        if (!RespawnObstacle)
+            Destroy(this.gameObject, 0.1f);
+    }
+}
