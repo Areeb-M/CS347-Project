7929b829809144a0daaf8934db57ecff06d89143
herpdaderpin <50278544+herpdaderpin@users.noreply.github.com>
2021-04-28 15:14:07 -0500

    Got the move left and right animations implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public int moveSpeed;
     public float thrust = 1.0f;
     public Rigidbody rb;
+    public Animator animator;
 
     //[Header("Set Dynamically")]
     public int jumpCount = 0;
@@ -24,6 +25,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Jump
         if (Input.GetKeyDown(KeyCode.Space))
         {
             //Checks if max jump count was reached
@@ -33,20 +35,33 @@ public class PlayerController : MonoBehaviour
                 rb.AddForce(0, thrust, 0, ForceMode.Impulse);
             }    
         }
+
+        //Move Left
         if (Input.GetKey(KeyCode.A))
         {
             transform.position = transform.position += transform.right * -moveSpeed * Time.deltaTime;
             Vector3 theScale = transform.localScale;
             theScale.x = -.2f;
             transform.localScale = theScale;
+            animator.SetFloat("Speed", 1);
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            animator.SetFloat("Speed", 0);
         }
 
+        //Move Right
         if (Input.GetKey(KeyCode.D))
         {
             transform.position = transform.position += transform.right * moveSpeed * Time.deltaTime;
             Vector3 theScale = transform.localScale;
             theScale.x = .2f;
             transform.localScale = theScale;
+            animator.SetFloat("Speed", 1);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            animator.SetFloat("Speed", 0);
         }
     }
 
